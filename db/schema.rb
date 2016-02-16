14fe25c9e3d2d22b19e545905505fbe337f5dbc8
Nick Martinez <23nicholasmartinez@gmail.com>
2016-02-16 19:06:11 +0000

    Create photo model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160214031111) do
+ActiveRecord::Schema.define(version: 20160216184243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 20160214031111) do
   end
 
   add_index "comments", ["user_id", "place_id"], name: "index_comments_on_user_id_and_place_id", using: :btree
+
+  create_table "photos", force: true do |t|
+    t.text     "caption"
+    t.integer  "place_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "photos", ["place_id"], name: "index_photos_on_place_id", using: :btree
 
   create_table "places", force: true do |t|
     t.string   "name"
